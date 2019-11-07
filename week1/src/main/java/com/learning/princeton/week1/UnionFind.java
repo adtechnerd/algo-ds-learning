a87e93588977166d5b08f8f44758f871ddfdc001
609734641 <aditya.ajmera@bt.com>
2019-11-07 12:11:20 +0530

    adding testng and basic package

---
+++ week1/src/main/java/com/learning/princeton/week1/UnionFind.java
@@ -0,0 +1,13 @@
+package com.learning.princeton.week1;
+
+public class UnionFind {
+
+	
+	public boolean connected(int a, int b) {
+		return false;
+	}
+	
+	public void find(int a, int b) {
+		
+	}
+}
---
+++ week1/src/test/java/com/learning/princeton/week1/TestUnionFind.java
@@ -0,0 +1,16 @@
+package com.learning.princeton.week1;
+
+import org.testng.annotations.Test;
+
+public class TestUnionFind {
+
+	@Test
+	public void connectedTest(int a, int b) {
+		
+	}
+	
+	@Test
+	public void findTest(int a, int b) {
+		
+	}
+}
