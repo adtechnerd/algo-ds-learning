bf8051d27037f274e00a2776c4fe1b5778a7d1cc
Aditya Ajmera <adityaajmera89@gmail.com>
2019-11-09 10:11:18 +0530

    creating union find client, union find data file

--- week1/src/main/java/com/learning/princeton/week1/UnionFindClient.java
+++ week1/src/main/java/com/learning/princeton/week1/UnionFindClient.java
@@ -3,6 +3,10 @@
  */
 package com.learning.princeton.week1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Client for UnionFind algorithms
  * @author aditya
@@ -10,8 +14,27 @@ package com.learning.princeton.week1;
  */
 public class UnionFindClient {
 
+	private static final String PATH = "src/main/resources/file/UnionFindData1";
+	
 	public static void main(String[] args) {
-		UnionFind uf = new QuickFind(10);
+		try {
+			Scanner in = new Scanner(new File(PATH));
+			int size = Integer.parseInt(in.nextLine());
+			System.out.println("size is: "+size);
+			UnionFind uf = new QuickFind(size);
+			
+			while(in.hasNext()) {
+				String pointPair = in.nextLine();
+				int a = Integer.parseInt(pointPair.split(" ")[0]);
+				int b = Integer.parseInt(pointPair.split(" ")[1]);
+				uf.connected(a, b);
+			}
+			in.close();
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
